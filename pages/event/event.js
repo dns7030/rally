209920720434d8fe6513b9a9a6fe350117cd97eb
unknown <denis.suslov@hotmail.com>
2020-08-13 21:48:35 +0800

    added new event page

--- pages/event/event.js
+++ pages/event/event.js
@@ -5,7 +5,9 @@ Page({
    * Page initial data
    */
   data: {
-    date: '2020-09-01'
+    date: '2020-09-01', 
+    event: {},
+    events: []
   },
 
   onLoad: function (options) {
@@ -15,7 +17,47 @@ Page({
     this.setData({
         date: e.detail.value
     })
-},
+  },
+  bindTimeChange(e) {
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  formSubmit: function (event) {
+    console.log('formSubmit', event);
+    console.log(event.detail.value.title)
+    console.log(event.detail.value.description)
+
+    let description = event.detail.value.description;
+    let title = event.detail.value.title;
+    let date1 = event.detail.value.date1;
+    console.log('date1', date1)
+    let date2 = event.detail.value.date2;
+    let date3 = event.detail.value.date3;
+    console.log('date2', date2)
+    console.log('date3', date3)
+    let events = new wx.BaaS.TableObject('events');
+    let newEvent = events.create();
+    const data = {
+      // restaurants_id: this.data.restaurants.id,
+      title: title,
+      description: description,
+      date: [date1, date2, date3]
+    }
+
+    newEvent.set(data);
+    // Post data to API
+    newEvent.save().then((res) => {
+      console.log('save res', res);
+      const newEvents = this.data.events;
+      newEvents.push(res.data);
+      this.setData({
+        event: newEvents,
+      })
+    })
+  },
+
+
   onReady: function () {
 
   },
