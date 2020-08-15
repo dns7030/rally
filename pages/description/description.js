c602cae79e4633ed110598f0ba223666f81625cb
a.b <anubvision@163.com>
2020-08-15 10:57:27 +0800

    Sorted timeline on past and future events

--- pages/description/description.js
+++ pages/description/description.js
@@ -1,18 +1,53 @@
 // pages/description/description.js
+let app= getApp()
 Page({
 
   /**
    * Page initial data
    */
   data: {
+    events: []
 
   },
 
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
+  userInfoHandler(data) {
+    wx.BaaS.auth.loginWithWechat(data).then(user => {
+        app.globalData.userInfo = user;
+        this.setData({
+          currentUser: user,
+        })
+      }, err => {
+    })
+  },
 
+  onLoad: function (options) {
+    this.setData({
+      currentUser: app.globalData.userInfo,
+    });
+    
+    const app = getApp();
+    console.log(app.globalData.userInfo);
+    this.setData({currentUser: app.globalData.userInfo});
+    const events = new wx.BaaS.TableObject('events');
+    events.find().then((res) => {
+      console.log('res',res)
+      //pull event data
+      let events = res.data.objects
+      //define event
+      let formatedEvents = []
+      //store event with time
+      events.forEach((event)=>{
+        console.log('event.date', event)
+        event.date = event.date.map(date => {
+          return util.formatTime(new Date(date));
+        });
+        // event.date = util.formatTime(new Date(event.date))
+        formatedEvents.push(event)
+      })
+      this.setData ({
+        events: formatedEvents
+      })
+    }); 
   },
 
   /**
